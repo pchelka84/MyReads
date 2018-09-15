f17922c6848df7705a53167601e185e4b04f832a
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-09-15 12:00:22 -0400

    Filtering through books to select those that are on the shelves

--- src/MainPage.js
+++ src/MainPage.js
@@ -10,9 +10,20 @@ class MainPage extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <BookShelf shelfName="Currently Reading" />
-            <BookShelf shelfName="Want to Read" />
-            <BookShelf shelfName="Read" />
+            <BookShelf
+              shelfName="Currently Reading"
+              books={this.props.filter(
+                book => book.shelf === "currentlyReading"
+              )}
+            />
+            <BookShelf
+              shelfName="Want to Read"
+              books={this.props.filter(book => book.shelf === "wantToRead")}
+            />
+            <BookShelf
+              shelfName="Read"
+              books={this.props.filter(book => book.shelf === "read")}
+            />
           </div>
         </div>
         <div className="open-search">
