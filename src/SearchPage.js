ab999b848fccdaa7c3c02ae3a7503e3c9612edf0
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-09-15 15:25:44 -0400

    Books from shelves display the correct shelf value on the selector

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -51,6 +51,11 @@ class SearchPage extends Component {
           <ol className="books-grid">
             {this.state.searchedBooks.map(searchedBook => {
               let shelf = "none";
+
+              this.props.books.map(
+                book =>
+                  book.id === searchedBook.id ? (shelf = book.shelf) : ""
+              );
               return (
                 <li key={searchedBook.id}>
                   <Book
