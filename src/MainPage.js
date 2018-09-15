9a669e341757a569fbbc5ccee5f4626976b8df1d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-09-15 11:26:37 -0400

    Code added to the components

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,6 +1,28 @@
 import React, { Component } from "react";
 import BookShelf from "./BookShelf";
 
-class MainPage extends Component {}
+class MainPage extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf />
+            <BookShelf />
+            <BookShelf />
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>
+            Add a book
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default MainPage;
