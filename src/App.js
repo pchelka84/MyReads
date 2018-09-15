35b5490d473422eba78efca36f685e440c1d288e
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-09-15 11:48:41 -0400

    Fetching books with componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import MainPage from "./MainPage";
 import SearchPage from "./SearchPage";
@@ -8,6 +8,12 @@ class BooksApp extends React.Component {
   state = {
     books: []
   };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
 
   render() {
     return (
