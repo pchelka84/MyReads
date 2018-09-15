81ef01e1295a3c7aa9ad4f8050f977988439a994
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-09-15 15:20:39 -0400

    Default value 'none' is shown for books with no shelf

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -49,11 +49,18 @@ class SearchPage extends Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.searchedBooks.map(searchedBook => (
-              <li key={searchedBook.id}>
-                <Book book={searchedBook} moveShelf={this.props.moveShelf} />
-              </li>
-            ))}
+            {this.state.searchedBooks.map(searchedBook => {
+              let shelf = "none";
+              return (
+                <li key={searchedBook.id}>
+                  <Book
+                    book={searchedBook}
+                    moveShelf={this.props.moveShelf}
+                    currentShelf={shelf}
+                  />
+                </li>
+              );
+            })}
           </ol>
         </div>
       </div>
