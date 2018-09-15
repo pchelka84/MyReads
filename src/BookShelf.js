98213440db35cdbc16629d231e5df8d2ecc99f75
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-09-15 11:15:46 -0400

    MainPage, SearchPage, BookShelf & Book components are added

---
+++ src/BookShelf.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import Book from "./Book";
+
+class BookShelf extends Component {}
+
+export default BookShelf;
---
+++ src/SearchPage.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+class SearchPage extends Component {}
+
+export default SearchPage;
