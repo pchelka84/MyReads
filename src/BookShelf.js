684fdbc033746df54496024687c4fbcd65c28457
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2018-09-16 09:28:26 -0400

    Refactoring the BookShelf class to a stateless functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,27 +1,25 @@
 import React, { Component } from "react";
 import Book from "./Book";
 
-class BookShelf extends Component {
-  render() {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelfName}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {this.props.books.map(book => (
-              <li key={book.id}>
-                <Book
-                  book={book}
-                  moveShelf={this.props.moveShelf}
-                  currentShelf={this.props.currentShelf}
-                />
-              </li>
-            ))}
-          </ol>
-        </div>
+function BookShelf(props) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.shelfName}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.map(book => (
+            <li key={book.id}>
+              <Book
+                book={book}
+                moveShelf={props.moveShelf}
+                currentShelf={props.currentShelf}
+              />
+            </li>
+          ))}
+        </ol>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default BookShelf;
